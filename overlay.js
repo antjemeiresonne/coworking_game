3ddd4ea9801c0e36d0db30b671832b39757448b5
Emmert-odisee <152487129+Emmert-odisee@users.noreply.github.com>
2024-06-03 20:29:54 +0200

    updates emmert
    de level lesector bij de rules zetten
    nu staat er "play" maar dan "level 1" "level 2" tot 3
    
    button weg
    
    timer functie

--- overlay.js
+++ overlay.js
@@ -1,27 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const overlay = document.getElementById('spelregels');
-    const openOverlayBtn = document.getElementById('show-rulesknop');
-    const closeOverlayBtnX = document.getElementById('closeOverlayBtnX');
-    const closeOverlayBtnPlay = document.getElementById('closeOverlayBtnPlay');
+document.addEventListener('DOMContentLoaded', function () {
+    const overlay = document.getElementById('spelregels')
+    const closeOverlayBtnX = document.getElementById('closeOverlayBtnX')
+    const overlayBtnPlay1 = document.getElementById('overlayBtnPlay1')
+    const overlayBtnPlay2 = document.getElementById('overlayBtnPlay2')
+    const overlayBtnPlay3 = document.getElementById('overlayBtnPlay3')
+    const endGame = document.getElementById('endGame')
 
-    overlay.style.display = 'flex';
+    overlay.style.display = 'flex'
 
-    openOverlayBtn.addEventListener('click', function() {
-        overlay.style.display = 'flex';
-    });
+    closeOverlayBtnX.addEventListener('click', function () {
+        overlay.style.display = 'none'
 
-    closeOverlayBtnX.addEventListener('click', function() {
-        overlay.style.display = 'none';
-    });
+    })
 
-    closeOverlayBtnPlay.addEventListener('click', function() {
-        overlay.style.display = 'none';
-    });
+    overlayBtnPlay1.addEventListener('click', function () {
+        overlay.style.display = 'none'
+        timer()
+    })
+
+    overlayBtnPlay2.addEventListener('click', function () {
+        overlay.style.display = 'none'
+        timer()
+    })
+
+    overlayBtnPlay3.addEventListener('click', function () {
+        overlay.style.display = 'none'
+        timer()
+    })
 
     // Close overlay when clicking outside of the content
-    overlay.addEventListener('click', function(event) {
-        if (event.target === overlay) {
-            overlay.style.display = 'none';
-        }
-    });
-});
+    // overlay.addEventListener('click', function (event) {
+    //     if (event.target === overlay) {
+    //         overlay.style.display = 'none'
+    //     }
+    // })
+
+    function timer() {
+        setTimeout(function () {
+            endGame.style.display = 'flex'
+        }, 5000) // = 30 seconds
+    }
+})
