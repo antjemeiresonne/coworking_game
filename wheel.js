2083659d0973a2bf69b9c4fef65323edd41f9ee5
Emmert-odisee <152487129+Emmert-odisee@users.noreply.github.com>
2024-06-13 08:21:53 +0200

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -9,7 +9,6 @@ const spinMessage = document.getElementById("spin-message");
 
 let hasSpun = false;
 
-
 openWheelButton.addEventListener("click", () => {
     wheelContainer.classList.remove("hidden");
     openWheelButton.classList.add("hidden");
